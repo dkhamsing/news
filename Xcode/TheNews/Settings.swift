a12a175318665914b2ab7ed5ed8fd905ebd393d2
dkhamsing <dkhamsing8@gmail.com>
2020-04-30 13:01:45 -0700

    Isolate UserDefault

--- Xcode/TheNews/Settings.swift
+++ Xcode/TheNews/Settings.swift
@@ -29,36 +29,10 @@ struct Settings {
     }
 }
 
-// Credits: https://www.avanderlee.com/swift/property-wrappers/
 struct UserDefaultsConfig {
     @UserDefault(Settings.CategoryKey, defaultValue: Settings.CategoryDefault)
     static var savedCategory: NewsCategory
 
     @UserDefault(Settings.StyleKey, defaultValue: Settings.StyleDefault)
     static var savedStyle: Style
-}
-
-@propertyWrapper
-struct UserDefault<T: Codable> {
-    let key: String
-    let defaultValue: T
-
-    init(_ key: String, defaultValue: T) {
-        self.key = key
-        self.defaultValue = defaultValue
-    }
-
-    var wrappedValue: T {
-        get {
-            guard
-                let saved = UserDefaults.standard.object(forKey: key) as? Data,
-                let decoded = try? JSONDecoder().decode(T.self, from: saved) else { return defaultValue }
-            return decoded
-        }
-        set {
-            if let encoded = try? JSONEncoder().encode(newValue) {
-                UserDefaults.standard.set(encoded, forKey: key)
-            }
-        }
-    }
 }
---
+++ Xcode/TheNews/UserDefault.swift
@@ -0,0 +1,35 @@
+//
+//  UserDefault.swift
+//  TheNews
+//
+//  Created by Daniel on 4/30/20.
+//  Copyright © 2020 dk. All rights reserved.
+//
+
+import Foundation
+
+// Credits: https://www.avanderlee.com/swift/property-wrappers/
+@propertyWrapper
+struct UserDefault<T: Codable> {
+    let key: String
+    let defaultValue: T
+
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            guard
+                let saved = UserDefaults.standard.object(forKey: key) as? Data,
+                let decoded = try? JSONDecoder().decode(T.self, from: saved) else { return defaultValue }
+            return decoded
+        }
+        set {
+            if let encoded = try? JSONEncoder().encode(newValue) {
+                UserDefaults.standard.set(encoded, forKey: key)
+            }
+        }
+    }
+}
