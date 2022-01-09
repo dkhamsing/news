fc7db9eac69804e0c0e62fff97da16c484a41f42
dkhamsing <dkhamsing8@gmail.com>
2022-01-09 08:43:29 -0800

    Revert Update TwitterCell
    11d76cc

--- Xcode/TheNews/Views/Twitter/TwitterCell.swift
+++ Xcode/TheNews/Views/Twitter/TwitterCell.swift
@@ -12,79 +12,75 @@ class TwitterCell: NewsCell {
     override func config() {
         super.config()
 
+        articleImageView.layer.cornerRadius = 10
+        articleImageView.layer.masksToBounds = true
+
         logo.backgroundColor = .secondarySystemBackground
         logo.layer.cornerRadius = TwitterCell.logoSize.width / 2
         logo.layer.masksToBounds = true
 
-        source.font = .preferredFont(forTextStyle: .headline)
-        ago.textColor = .secondaryLabel
-
-        let topView = UIView()
-
-        [topView, articleImageView, title].forEach { item in
+        [logo, source, articleImageView, title].forEach { item in
             contentView.addSubviewForAutoLayout(item)
         }
 
-        let rightView = UIView()
-
-        [logo, rightView].forEach { item in
-            topView.addSubviewForAutoLayout(item)
-        }
-
-        [source, ago].forEach { item in
-            rightView.addSubviewForAutoLayout(item)
-        }
-
         let imageHeight: CGFloat = 200
+        let inset: CGFloat = 10
         NSLayoutConstraint.activate([
-            topView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-            topView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
-            topView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
-            topView.heightAnchor.constraint(equalToConstant: TwitterCell.logoSize.height),
+            logo.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            logo.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            logo.widthAnchor.constraint(equalToConstant: TwitterCell.logoSize.width),
+            logo.heightAnchor.constraint(equalToConstant: TwitterCell.logoSize.width),
 
-            title.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 8),
-            title.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
+            source.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            source.leadingAnchor.constraint(equalTo: logo.trailingAnchor, constant: inset),
+            contentView.readableContentGuide.trailingAnchor.constraint(equalTo: source.trailingAnchor),
+
+            title.topAnchor.constraint(equalTo: source.bottomAnchor, constant: 2),
+            title.leadingAnchor.constraint(equalTo: logo.trailingAnchor, constant: inset),
             contentView.readableContentGuide.trailingAnchor.constraint(equalTo: title.trailingAnchor),
 
-            articleImageView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 8),
-            articleImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: articleImageView.trailingAnchor),
+            articleImageView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: inset),
+            articleImageView.leadingAnchor.constraint(equalTo: logo.trailingAnchor, constant: inset),
+            contentView.readableContentGuide.trailingAnchor.constraint(equalTo: articleImageView.trailingAnchor),
             articleImageView.heightAnchor.constraint(equalToConstant: imageHeight),
 
             contentView.bottomAnchor.constraint(equalTo: articleImageView.bottomAnchor, constant: 15)
-        ])
-
-        NSLayoutConstraint.activate([
-            logo.leadingAnchor.constraint(equalTo: topView.leadingAnchor),
-            logo.widthAnchor.constraint(equalToConstant: TwitterCell.logoSize.width),
-            logo.heightAnchor.constraint(equalToConstant: TwitterCell.logoSize.width),
-            logo.centerYAnchor.constraint(equalTo: topView.centerYAnchor),
-
-            rightView.leadingAnchor.constraint(equalTo: logo.trailingAnchor, constant: 5),
-            rightView.trailingAnchor.constraint(equalTo: topView.trailingAnchor),
-            rightView.centerYAnchor.constraint(equalTo: topView.centerYAnchor)
-        ])
-
-        NSLayoutConstraint.activate([
-            source.topAnchor.constraint(equalTo: rightView.topAnchor),
-            source.leadingAnchor.constraint(equalTo: rightView.leadingAnchor),
-            source.trailingAnchor.constraint(equalTo: rightView.trailingAnchor),
-
-            ago.topAnchor.constraint(equalTo: source.bottomAnchor),
-            ago.leadingAnchor.constraint(equalTo: rightView.leadingAnchor),
-            ago.trailingAnchor.constraint(equalTo: rightView.trailingAnchor),
-            ago.bottomAnchor.constraint(equalTo: rightView.bottomAnchor)
         ])
     }
 
     func load(article: Article, downloader: ImageDownloader) {
         title.text = article.titleDisplay
-        source.text = article.source?.name
-        ago.text = article.ago
+        source.attributedText = article.twitterAttributedFirstLine
 
         load(urlString: article.urlToImage, downloader: downloader)
 
         loadLogo(urlString: article.urlToSourceLogo, size: TwitterCell.logoSize, downloader: downloader)
+    }
+
+}
+
+private extension Article {
+
+    var twitterAttributedFirstLine: NSAttributedString {
+        guard let name = source?.name else { return NSAttributedString() }
+
+        let nameAttribute: [NSAttributedString.Key: Any] = [
+            .font: UIFont.boldSystemFont(ofSize: 17)
+        ]
+        let a = NSMutableAttributedString(string: name, attributes: nameAttribute)
+
+        var str = ""
+        if let ago = ago {
+            str = " · \(ago)"
+        }
+        let titleAttribute: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 17),
+            .foregroundColor: UIColor.gray
+        ]
+        let b = NSAttributedString(string: str, attributes: titleAttribute)
+        a.append(b)
+
+        return a
     }
 
 }
